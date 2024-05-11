d0bbb7aab92dda144fcbacfed00be95366060a78
Kevin Le <89573171+kevle6@users.noreply.github.com>
2024-05-10 22:25:21 -0700

    Update main.py
    Edits made by Professor Ryan Bakonis.
    
    The new code is able to pull reports using a two-stage process. This doesn't require the base64 encoding of the URL as it submits the URL as form data.

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-# import time
-# import json
 import pandas
 import os
-import base64
 
-def url_to_base64(url):
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    response = requests.get(url)
-    if response.status_code == 200:
-        return base64.urlsafe_b64encode(response.content).decode('utf-8')
-    else:
-        return None
+api_key = "4d9049854df6fc33acb24daf429e1c036bad5b2de61485e5c8a17ca3d72d818a"
 
 while True:
     try:
@@ -28,55 +13,42 @@ while True:
         pass
     print("File Does Not Exist")
 
-domain_csv = pandas.read_csv((file_path))
 
+domain_csv = pandas.read_csv(file_path)
 urls = domain_csv['Domain'].tolist()
 
-# parameters = {'apikey': my_api_key, 'resource': urls}
 
-base64list = []
+# Create an empty list to record the scan IDs for URLs in the spreadsheet
+report_urls = []
 
 for i in urls:
-    # if not i.startswith("http://") or not i.startswith("https://"):
-    #     i = 'http://' + i
-    # parameters = {'apikey': my_api_key, 'resource': i}
+    url = "https://www.virustotal.com/api/v3/urls"
 
-    # payload = { "url": i }
-    # url = "https://www.virustotal.com/api/v3/urls/"
-    # headers = {
-    #     "accept": "application/json",
-    #     "x-apikey": "4d9049854df6fc33acb24daf429e1c036bad5b2de61485e5c8a17ca3d72d818a",
-    #     "content-type": "application/x-www-form-urlencoded"
-    # }
+    payload = { "url": i }
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "x-apikey": api_key
+    }
 
-    # response = requests.post(url, data=payload, headers=headers)
-    url_to_base64(i)
-    base64_string = url_to_base64(i)
-    if base64_string != None:
-        base64list.append(base64_string)
+    # Fetch the scan ID for a specific URL and append it to the report_urls list
+    response = requests.post(url, data=payload, headers=headers)
+    response_json = response.json()
 
-for i in base64list:
-    url = "https://www.virustotal.com/api/v3/urls/" + base64_string
+    if "data" in response.json():
+        report_urls.append(response_json['data']['links']['self'])
+
+
+# Iterate over each of the scan IDs to obtain the report associated with each URL
+for url in report_urls:
 
     headers = {
         "accept": "application/json",
-        "x-apikey": "4d9049854df6fc33acb24daf429e1c036bad5b2de61485e5c8a17ca3d72d818a",
+        "x-apikey": api_key
     }
     response = requests.get(url, headers=headers)
+    response_json = response.json()
 
-    print(response.text)
-    
-    # if json_response['code'] == 'NotFoundError':
-    if json_response['error']['code'] == 'NotFoundError': 
-        with open('Unknown Links.txt', 'a')  as notfound:
-            notfound.write(i) and notfound.write("\tNOT found: Requires Manual Scan\n")
-    # elif json_response['code'] >= 1:
-    else:
-        if json_response['code'] <= 0:
-            with open('VirusTotal_Clean_Links.txt', 'a')  as clean:
-                clean.write(i) and clean.write("\t NOT Malicious \n")
-        else:
-            with open('VirusTotal_Malicious_Links.txt', 'a')  as malicious:
-                malicious.write(i) and malicious.write("\t Malicious") and malicious.write("\t Domains Detected by   "+ str(json_response['positives']) + "  Solutions\n")
-
-    # time.sleep(15)
+    print(response_json['meta']['url_info']['url'])
+    # Could also change ['stats'] to ['results'] for more detailed output
+    for k,v in response_json['data']['attributes']['stats'].items():
+        print(f"{k}: {v}")
